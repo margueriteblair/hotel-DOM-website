22760decdd780c0c996523ae655d6ee55c1fea8b
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-01 23:11:09 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+General repo for June 2020;
---
+++ hotelflex.js
@@ -0,0 +1,19 @@
+//display 3 rooms available with dates;
+
+let roomList = [101, 102, 103, 201, 202, 203, 301, 302, 303]
+
+let html = "<select>"
+for (let i = 0; i < roomList.length; i++) {
+        html = html + `<option value="${roomList[i]}">${roomList[i]}</option>`
+        console.log(roomList[i])
+}
+
+    html = html + "</select>"
+    document.getElementById("dropdown").innerHTML = html;
+
+let bookedRooms = [];
+let bookBtn = document.getElementById("book")
+bookBtn.addEventListener("click", function(){
+    bookedRooms.push(html.value)
+    roomList.splice(roomList.indexOf(html.value))
+})
